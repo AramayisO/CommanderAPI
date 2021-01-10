edb8833cb9a461b8f874c5e6cedb4309fe72cd58
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-01-09 16:42:53 -0800

    Add a simple controller with endpoints to get commands

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,38 @@
+﻿using CommanderAPI.Models;
+using CommanderAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderAPI.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepository _commandRepository;
+
+        public CommandsController(ICommandRepository commandRepository)
+        {
+            _commandRepository = commandRepository;
+        }
+
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command >> GetAllCommands()
+        {
+            var commands = _commandRepository.GetAllCommands();
+            return Ok(commands);
+        }
+
+        // /GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var command = _commandRepository.GetCommandById(id);
+            return Ok(command);
+        }
+    }
+}
