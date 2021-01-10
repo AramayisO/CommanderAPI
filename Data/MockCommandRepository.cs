0ba1c408aee461cd66627e1226bb366ac8f3d924
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-01-09 16:41:42 -0800

    Use the repository pattern to access commands

---
+++ Data/MockCommandRepository.cs
@@ -0,0 +1,28 @@
+﻿using CommanderAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderAPI.Data
+{
+    public class MockCommandRepository : ICommandRepository
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{ Id=0, HowTo="List contents of the current direcotry", Line="dir", Platform="Windows" },
+                new Command{ Id=1, HowTo="Print current working directory", Line="pwd", Platform="Unix" },
+                new Command{ Id=2, HowTo="Update software", Line="sudo apt update", Platform="Unix" }
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "List contents of the current direcotry", Line = "dir", Platform = "Windows" };
+        }
+    }
+}
